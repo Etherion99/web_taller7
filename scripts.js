d2f8dbc908c732173a352c4629c41b2d2f8e70d6
Etherion99 <37667366+Etherion99@users.noreply.github.com>
2021-09-26 12:14:08 -0500

    validaciones previas

--- scripts.js
+++ scripts.js
@@ -51,5 +51,101 @@ $(document).ready(function(){
     
 });
 
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const formObj = Object.fromEntries(formData);
+
+    console.log(formObj);
+
+    const name = document.getElementById('name');
+    const lastname = document.getElementById('lastname');
+    const country = document.getElementById('country');
+    const department = document.getElementById('department');
+    const municipio = document.getElementById('municipio');
+    const username = document.getElementById('username');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const password2 = document.getElementById('password2');
+
+    validation(
+        name, [formObj['name'] == '', formObj['name'].length > 25], ['Campo obligatorio', 'Longitud máxima: 25']
+    );
+
+    validation(
+        lastname, [formObj['lastname'] == '', formObj['lastname'].length > 25], ['Campo obligatorio', 'Longitud máxima: 25']
+    );
+
+    validation(
+        country, [formObj['country'] == ''], ['Campo obligatorio']
+    );
+
+    validation(
+        department, [formObj['department'] == ''], ['Campo obligatorio']
+    );
+
+    validation(
+        municipio, [formObj['municipio'] == ''], ['Campo obligatorio']
+    );
+
+    validation(
+        username, [
+            formObj['username'] == '',
+            formObj['username'].length > 20,
+            formObj['username'].length < 10,
+            formObj['username'].match(/^[a-zA-Z0-9]+$/)==null
+        ], [
+            'Campo obligatorio',
+            'La longitud debe estar entre 10 y 20 carácteres',
+            'La longitud debe estar entre 10 y 20 carácteres',
+            'El nombre de usuario no debe tener carácteres especiales'
+        ]
+    );
+
+    validation(
+        password, [
+            formObj['password'] == '',
+            formObj['password'].length > 20,
+            formObj['password'].length < 10,
+            formObj['password'].match(new RegExp('(?=.*[!#%/&])'))==null
+        ], [
+            'Campo obligatorio',
+            'La longitud debe estar entre 10 y 20 carácteres',
+            'La longitud debe estar entre 10 y 20 carácteres',
+            'La contraseña debe contener al menos de uno de estos carácteres especiales #,%,/,&'
+        ]
+    );
+
+    validation(
+        password2, [
+            formObj['password2'] == '',
+            formObj['password2'] != formObj['password']
+        ], [
+            'Campo obligatorio',
+            'Las contraseñas deben coincidir'
+        ]
+    );
+});
+
+function validation(input, conditions, messages) {
+    for (let i in conditions) {
+        let condition = conditions[i];
+
+        if (condition) {
+            input.classList.add('is-invalid');
+            input.classList.remove('is-valid');
+            input.parentNode.querySelector('.invalid-feedback').innerText = messages[i];
+            break;
+        } else {
+            input.classList.add('is-valid');
+            input.classList.remove('is-invalid');
+            input.parentNode.querySelector('.invalid-feedback').innerText = '';
+        }
+    }
+}
+
 
 
